b558d5e42045d64468f1e65ca72d6328c95a15ca
shaunmartinx <shaunmartinxj@gmail.com>
2026-03-17 14:17:17 +0530

    Update feature_pp1.java

--- feature_pp1.java
+++ feature_pp1.java
@@ -7,3 +7,22 @@ class Age {
         System.out.println("Harry's age in 2024 is " + age);
     }
 }
+class feature_pp1i{
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sam's average mark in PCM is " + average);
+    }
+}
+public class feature_pp1ii {
+    public static void main(String[] args) {
+        double km = 10.8;
+        double miles = km * 1.6;
+
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}
